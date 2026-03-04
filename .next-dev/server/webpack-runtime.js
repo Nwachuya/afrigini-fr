c8ccc29a37aab2708c9f618fa3ec9493c0673cd1
Nwachuya <106511230+Nwachuya@users.noreply.github.com>
2026-03-04 19:16:52 +0100

    mnm

--- .next-dev/server/webpack-runtime.js
+++ .next-dev/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("d691cea9ae52592c")
+/******/ 		__webpack_require__.h = () => ("b55edd1c1bd43e43")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
